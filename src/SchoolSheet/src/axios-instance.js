6dd0678de2f841cec2c1581a2932c29f4f56f10d
jim-junior <jimjunior854@gmail.com>
2023-10-14 17:21:41 +0300

    Add heroku api

--- src/SchoolSheet/src/axios-instance.js
+++ src/SchoolSheet/src/axios-instance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 //const HOST_URL = "https://schoolsoftapp-3432013afef6.herokuapp.com";
 //const HOST_URL = "http://192.168.43.1:3001";
 
-const HOST_URL = "https://fellow-ship-app.onrender.com";
+const HOST_URL = "https://fellowship-41b7b8c7191e.herokuapp.com";
 
 
 export const UPLOADS_URL = HOST_URL + "/useruploads";
